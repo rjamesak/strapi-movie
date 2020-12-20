7a4e3c6ff4b81e6341098f2363d494dab85d4c19
Russell James <jamesru@oregonstate.edu>
2020-12-19 20:18:29 -0900

    first, input box send movie title to strapi

---
+++ src/components/movieFetcher.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        Enter a movie to add:
+        <input type="text" v-model="movieName"/>
+        <button @click="addMovie" type="submit">Submit</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MovieFetcher",
+        data() {
+            return {
+                movieName: ''
+            }
+        },
+        methods: {
+            addMovie() {
+                console.log('title: ', this.movieName)
+                fetch('http://localhost:1337/movies', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({title: this.movieName})
+                })
+                .then(response => response.json())
+                .then(data => console.log(data))
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MovieFetcher/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MovieFetcher from '@/components/movieFetcher.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MovieFetcher
   }
 }
 </script>
